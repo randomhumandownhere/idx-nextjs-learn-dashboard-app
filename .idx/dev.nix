2bd22fa17789e1020dde5a9f36b2cd31ae7a558b
Giacomo F.M <giacomo.fraron@avvale.com>
2024-07-19 16:49:33 +0200

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -11,18 +11,13 @@
       "alphabotsec.vscode-eclipse-keybindings"
     ];
     workspace = {
-      # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
-        clone-starter = "npx create-next-app@latest nextjs-dashboard --example \"https://github.com/vercel/next-learn/tree/main/dashboard/starter-example\" --use-pnpm";
-        cd-dashboard = "cd nextjs-dashboard";
-        npm-install = "pnpm i";
+        commands = "npx --yes create-next-app@latest nextjs-dashboard --example \"https://github.com/vercel/next-learn/tree/main/dashboard/starter-example\" --use-pnpm | cd nextjs-dashboard | pnpm i";
       };
-      # To run something each time the workspace is (re)started, use the `onStart` hook
       onStart = {
         cd-dashboard = "cd nextjs-dashboard";
       };
     };
-    # Enable previews and customize configuration
     previews = {
       enable = true;
       previews = {
